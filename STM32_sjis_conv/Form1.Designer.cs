af5d6ded046363b58f760424c6763d82ed4a3a04
sima <46996330+sima2022@users.noreply.github.com>
2023-04-01 14:52:44 +0900

    Add files via upload

---
+++ STM32_sjis_conv/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace STM32_sjis_conv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "sjis_変換";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(229, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "全行数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(305, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(336, 499);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "変換前";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "変換後";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(381, 100);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(336, 499);
+            this.listBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(730, 601);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ STM32_sjis_conv/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace STM32_sjis_conv
+{
+    public partial class Form1 : Form
+    {
+       String str,str1,NG1str,NG2str,strbuf;
+       public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("ここにメッセージ", "ここにタイトル");
+            int i = 0;
+            openFileDialog1.FileName = "*.txt";
+            strbuf = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // ファイルのパスを取得する
+                string filePath = openFileDialog1.FileName;
+
+                // ファイルを開く
+                StreamReader reader = new StreamReader(filePath);
+
+                try
+                {
+                    // ファイルを一行ずつ読み込む
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        i++;// Console.WriteLine(line);
+#if true
+
+                        //if (i > 1000)
+                        //{
+                        //    break;
+                        //}
+                        //else
+                        //{
+                            listBox1.Items.Add(line);
+                            NG1str = "------"; NG2str = "UTF-8";
+                            if (line.Contains(NG1str) || line.Contains(NG2str))
+                            {
+                                //Console.WriteLine(“指定した文字列が含まれています。”);
+                            }
+                            else
+                            {
+                                //Console.WriteLine(“指定した文字列がありません。”);
+                                str1 = line;//01 09 2129 8148 A1A9 EFBC9F FF1F   ？
+                                str = str1.Remove(0, 11);//8148 A1A9 EFBC9F FF1F   ？
+                                str1 = str.Remove(4, 12);//8148 FF1F   ？
+                                str = str1.Insert(0, "{0x");//{0x8148 FF1F   ？
+                                str1 = str.Remove(7, 1);//{0x8148FF1F   ？
+                                str = str1.Insert(7, ",0x");//{0x8148,0xFF1F   ？
+                                str1 = str.Insert(14, "},//");//{0x8148,0xFF1F},//   ？
+                                str1 += '\r';
+                                listBox2.Items.Add(str1);// label2.Text = line;
+                                strbuf += str1;
+                            }
+                        //}
+#endif
+#if false
+listBox1.Items.Add(line);
+#endif
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("エラー: " + ex.Message);
+                }
+                finally
+                {
+                    str = i.ToString();
+                    label1.Text = str;
+                    MessageBox.Show(str, "全行数");
+                    // ファイルを閉じる
+                    reader.Close();
+                }
+                saveFileDialog1.FileName = "stmtable.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    System.IO.Stream stream;
+                    stream = saveFileDialog1.OpenFile();
+                    if (stream != null)
+                    {
+                        //ファイルに書き込む
+                        System.IO.StreamWriter sw = new System.IO.StreamWriter(stream);
+                        sw.Write(strbuf);
+                        //閉じる
+                        sw.Close();
+                        stream.Close();
+
+                    }
+                }
+            }
+        }
+    }
+}
